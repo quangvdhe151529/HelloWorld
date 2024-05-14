2bbfc0801656d3787a853a5533d613556117d094
quangvdhe151529 <quangvdhe151529@fpt.edu.vn>
2024-05-15 00:00:31 +0700

    update slot 3

--- app/src/main/java/slot3/Demo31MainActivity.java
+++ app/src/main/java/slot3/Demo31MainActivity.java
@@ -24,10 +24,10 @@ public class Demo31MainActivity extends AppCompatActivity {
      setContentView(R.layout.activity_demo31_main);
      lv = findViewById(R.id.slot3listview);
      //nguon du lieu
-        String arr = new String[]{"Mon 1","Slot 3","Mon 2","Mon 3","Mon 4","Mon 5"};
+        String[] arr=new String[]{"Mon 1","Slot 3","Mon 2","Mon 3","Mon 4","Mon 5"};
         //adapter
         ArrayAdapter<String>
-                adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,arr);
+                adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, arr);
         //hien thi listview
         lv.setAdapter(adapter);
     }
