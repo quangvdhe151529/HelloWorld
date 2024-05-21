b34209f1e6689058e0209b3a25ce240c22ddd2e1
quangvdhe151529 <quangvdhe151529@fpt.edu.vn>
2024-05-21 23:21:55 +0700

    update

--- app/src/main/java/slot5/Demo51MainActivity.java
+++ app/src/main/java/slot5/Demo51MainActivity.java
@@ -23,11 +23,17 @@ public class Demo51MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_demo51_main);
         listView=findViewById(R.id.demo51Listview1);
+        // Demo51SqliteHelper helper = new Demo51SqliteHelper(this);
+        // SQLiteDatabase db = helper.getReadableDatabase();
         Demo51ProductDAO dao=new Demo51ProductDAO(this);
+        // Demo51Product p =new Demo51Product("1", "San pham 1",123,1);
+        // int kq=dao.insertProduct(p);
         list=dao.getAll();
         adapter = new Demo51Adapter(list,this);
         listView.setAdapter(adapter);
+        // int kq=dao.insertProduct(p);
     }
 }
